e86f2ba277e3ccefc3f70992dbd7e6830152253f
Marcin Trajfacki <kirinson321@gmail.com>
2023-10-01 17:43:22 +0200

    Add interfaces definitions

--- pkg/domain/rate.go
+++ pkg/domain/rate.go
@@ -1,6 +1,32 @@
 package domain
 
+import (
+	"context"
+	"time"
+)
+
 type (
+	ExchangeService interface {
+		GetRates(ctx context.Context) error
+	}
+
+	ExchangeDownloader interface {
+		GetRates(ctx context.Context)
+	}
+
+	Outputter interface {
+		Output(o StructuredOutput) error
+	}
+
+	StructuredOutput struct {
+		OutputTimestamp     time.Time     `json:"outputTimestamp"`
+		RequestDuration     int64         `json:"rquestDuration"`
+		ResponseHTTPCode    string        `json:"respHTTPCode"`
+		ResponseContentType string        `json:"respContentType"`
+		ResponseValidJSON   bool          `json:"respValidJSON"`
+		RatesData           ExchangeRates `json:"ratesData"`
+	}
+
 	ExchangeRates struct {
 		Table        string       `json:"table"`
 		Currency     string       `json:"currency"`
--- pkg/exchange/service.go
+++ pkg/exchange/service.go
@@ -1,1 +1,23 @@
 package exchange
+
+import (
+	"context"
+
+	"github.com/kirinson321/bsg-recruitment/pkg/domain"
+)
+
+type service struct {
+	exchangeDownloader domain.ExchangeDownloader
+	outputter          domain.Outputter
+}
+
+func NewService(exchangeDownloader domain.ExchangeDownloader, outputter domain.Outputter) domain.ExchangeService {
+	return &service{
+		exchangeDownloader: exchangeDownloader,
+		outputter:          outputter,
+	}
+}
+
+func (s *service) GetRates(ctx context.Context) error {
+	return nil
+}
